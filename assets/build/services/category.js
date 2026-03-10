009ed9fcdd18157447b54d54d3ecf06a2191e875
jeperico <joaoeperico@gmail.com>
2026-03-10 16:56:32 -0300

    feat: creating category service

--- assets/build/services/category.js
+++ assets/build/services/category.js
@@ -1,6 +1,18 @@
-"use strict";
 const createCategory = (event) => {
     event.preventDefault();
     alert("TESTE");
+    const category = document.querySelector("#category");
+    const tax = document.querySelector("#tax");
+    // TODO: Validate inputs
+    if (!category || !tax)
+        return;
+    // TODO: Handle values
+    const payload = {
+        id: 1,
+        name: category.value,
+        tax: parseInt(tax.value) || 0,
+        is_active: true,
+    };
+    localStorage.setItem("category", JSON.stringify(payload));
 };
-// export default createCategory;
+export {};
--- assets/services/category.ts
+++ assets/services/category.ts
@@ -1,6 +1,23 @@
+import { ICategory } from "../interfaces/category";
+
 const createCategory = (event: SubmitEvent) => {
   event.preventDefault();
   alert("TESTE");
-};
 
-// export default createCategory;
+  const category = document.querySelector("#category") as HTMLInputElement;
+  const tax = document.querySelector("#tax") as HTMLInputElement;
+
+  // TODO: Validate inputs
+  if (!category || !tax) return;
+
+  // TODO: Handle values
+
+  const payload: ICategory = {
+    id: 1,
+    name: category.value,
+    tax: parseInt(tax.value) || 0,
+    is_active: true,
+  };
+
+  localStorage.setItem("category", JSON.stringify(payload));
+};
